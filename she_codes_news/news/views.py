9453a963bde0fa39fb47cfaeedcb8177aafe85a1
monelith <monelle.trachez@gmail.com>
2022-02-04 22:07:40 +0800

    Update 04.02.22 - NewsStory Forms completed

--- she_codes_news/news/views.py
+++ she_codes_news/news/views.py
@@ -1,5 +1,11 @@
+from audioop import reverse
+from difflib import context_diff
+from statistics import mode
+from traceback import format_exception_only
 from django.views import generic
+from django.urls import reverse_lazy
 from .models import NewsStory
+from .forms import StoryForm
 
 
 class IndexView(generic.ListView):
@@ -14,3 +20,16 @@ class IndexView(generic.ListView):
         context['latest_stories'] = NewsStory.objects.all()[:4]
         context['all_stories'] = NewsStory.objects.all()
         return context
+
+
+class StoryView(generic.DetailView):
+    model = NewsStory
+    template_name = 'news/story.html'
+    context_object_name = 'story'
+
+
+class AddStoryView(generic.CreateView):
+    form_class = StoryForm
+    context_object_name = 'storyForm'
+    template_name = 'news/createStory.html'
+    success_url = reverse_lazy('news:index')
