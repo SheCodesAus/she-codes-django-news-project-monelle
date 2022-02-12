2480f52f06f8200710d452bafe859f923036e7e6
monelith <monelle.trachez@gmail.com>
2022-02-12 09:35:49 +0800

    Update 12.02.22
    On UserProfile page,  displays posts by User

--- she_codes_news/users/urls.py
+++ she_codes_news/users/urls.py
@@ -1,11 +1,9 @@
 from django.urls import path
-from .views import CreateAccountView, UserProfile
+from .views import CreateAccountView, UserProfile, UpdateView
 
 app_name = 'users'
 
 urlpatterns = [ 
-    path('create-account/', CreateAccountView.as_view(),
-name='createAccount'),
-    path('userProfile/', UserProfile.as_view(),    
-name='userProfile'),
+    path('create-account/', CreateAccountView.as_view(), name='createAccount'),
+    path('userProfile/', UserProfile.as_view(), name='userProfile'),
 ]
--- she_codes_news/users/views.py
+++ she_codes_news/users/views.py
@@ -1,9 +1,11 @@
+from wsgiref.handlers import format_date_time
 from django.shortcuts import render
 
 from django.urls import reverse_lazy
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView
 from django.views import generic
 from .models import CustomUser
+from news.models import NewsStory
 from .forms import CustomUserCreationForm
 
 
@@ -16,3 +18,9 @@ class UserProfile(CreateView):
     form_class = CustomUserCreationForm
     success_url = reverse_lazy('userProfile')
     template_name = 'users/userprofile.html'
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['user_stories'] = NewsStory.objects.filter(author=self.kwargs['pk'])
+        return context
+
