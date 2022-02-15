c8059a48b36ee6519ea053d35cbbcf27dbf9648c
monelith <monelle.trachez@gmail.com>
2022-02-15 19:33:46 +0800

    Same as previosu

--- she_codes_news/users/forms.py
+++ she_codes_news/users/forms.py
@@ -1,5 +1,4 @@
 from dataclasses import field
-from socket import fromshare
 from django import forms
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from .models import CustomUser
