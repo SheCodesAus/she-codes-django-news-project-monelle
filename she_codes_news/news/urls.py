993223f0b8ffdb916c7ae3f1e22be4af0aaec01b
monelith <monelle.trachez@gmail.com>
2022-02-19 10:40:03 +0800

    Delete Post function working

--- she_codes_news/news/urls.py
+++ she_codes_news/news/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
     path('<int:pk>/', views.StoryView.as_view(), name='story'),
     path('add-story/', views.AddStoryView.as_view(), name='newStory'),
-    path('<int:pk>/delete', views.DeleteStoryView.as_view(), name='delete'),
+    path('<int:pk>/delete/', views.DeleteStoryView.as_view(), name='delete'),
 ]
